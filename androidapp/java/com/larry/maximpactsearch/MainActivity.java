9c747f5f199d762d92364d74d6b7eda5112bb23e
llarryyllarryy <101638127+llarryyllarryy@users.noreply.github.com>
2022-05-19 08:51:56 +0200

    Add files via upload

--- androidapp/java/com/larry/maximpactsearch/MainActivity.java
+++ androidapp/java/com/larry/maximpactsearch/MainActivity.java
@@ -1,1 +1,56 @@
+package com.larry.maximpactsearch;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        webView = (WebView)findViewById(R.id.web);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webView.setLongClickable(true);
+
+
+
+
+
+
+
+        webView.loadUrl("https://llarryyllarryy.github.io/Max-Impact-Search/?mobile=true");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
